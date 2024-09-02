417a604a5659566aae068203a8f649b796b94fae
Piotr S <177131800+pedrosmake@users.noreply.github.com>
2024-09-02 11:52:13 +0200

    US000: data inserts for easier testing

---
+++ src/main/resources/db/migration/V4__insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO capability (capabilityName) VALUES ("delivery");
+INSERT INTO band (bandName) VALUES ("Architect");
+INSERT INTO job_roles (roleName, location,capabilityId, bandId, closingDate, status) VALUES
+    ("Software Engineer (T)", "Buenos Aires", 1, 1, '2019-01-01', 'open'),
+    ("Software Engineer (T)", "Buenos Aires", 1, 1, '2019-01-01', 'closed')
+;
