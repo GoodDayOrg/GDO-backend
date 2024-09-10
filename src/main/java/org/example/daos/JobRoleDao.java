1dd8836e84fee64500901a1b79c3ce5e8203f4ce
JakubW-k <jakub.wiecek@kainos.com>
2024-09-10 13:26:17 +0200

    jobRoleDao methods

--- src/main/java/org/example/daos/JobRoleDao.java
+++ src/main/java/org/example/daos/JobRoleDao.java
@@ -8,13 +8,15 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import org.example.exceptions.ResultSetException;
 import org.example.models.JobRole;
 import org.example.models.JobRoleDetails;
 import org.example.models.JobRoleFilteredRequest;
 
 public class JobRoleDao {
-    public List<JobRole> getAllJobRoles() throws SQLException, ResultSetException {
+    public List<JobRole> getAllJobRoles()
+            throws SQLException, ResultSetException {
         List<JobRole> jobRoles = new ArrayList<>();
 
         try (Connection connection = DatabaseConnector.getConnection()) {
@@ -30,7 +32,8 @@ public class JobRoleDao {
         return jobRoles;
     }
 
-    public List<JobRole> getFilteredJobRoles(final JobRoleFilteredRequest jobRequest)
+    public List<JobRole> getFilteredJobRoles(
+            final JobRoleFilteredRequest jobRequest)
             throws SQLException, ResultSetException {
         List<JobRole> jobRoles = new ArrayList<>();
         StringBuilder query = new StringBuilder(baseQuery());
@@ -53,13 +56,17 @@ public class JobRoleDao {
     }
 
     private void applyFiltersToQuery(
-            final JobRoleFilteredRequest jobRequest, final StringBuilder query, final List<Object> parameters) {
-        if (jobRequest.getRoleName() != null && !jobRequest.getRoleName().isBlank()) {
+            final JobRoleFilteredRequest jobRequest, final StringBuilder query,
+            final List<Object> parameters) {
+        if (jobRequest.getRoleName() != null &&
+                !jobRequest.getRoleName().isBlank()) {
             query.append(" AND roleName LIKE ?");
             parameters.add(jobRequest.getLikeRoleName());
         }
-        applyFilter(jobRequest.getJobRoleLocation(), "location", query, parameters);
-        applyFilter(jobRequest.getCapabilityName(), "capabilityName", query, parameters);
+        applyFilter(jobRequest.getJobRoleLocation(), "location", query,
+                parameters);
+        applyFilter(jobRequest.getCapabilityName(), "capabilityName", query,
+                parameters);
         applyFilter(jobRequest.getBandName(), "bandName", query, parameters);
         if (jobRequest.getClosingDate() != null) {
             query.append(" AND closingDate < ?");
@@ -70,17 +77,20 @@ public class JobRoleDao {
     }
 
     private void executeFilteredJobQuery(
-            final StringBuilder query, final List<Object> parameters, final List<JobRole> jobRoles)
+            final StringBuilder query, final List<Object> parameters,
+            final List<JobRole> jobRoles)
             throws SQLException, ResultSetException {
         try (Connection connection = DatabaseConnector.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(query.toString())) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    query.toString())) {
                 for (int i = 0; i < parameters.size(); i++) {
                     if (parameters.get(i) instanceof String) {
                         statement.setString(i + 1, (String) parameters.get(i));
                     } else if (parameters.get(i) instanceof Integer) {
                         statement.setInt(i + 1, (Integer) parameters.get(i));
                     } else if (parameters.get(i) instanceof java.sql.Date) {
-                        statement.setDate(i + 1, (java.sql.Date) parameters.get(i));
+                        statement.setDate(i + 1,
+                                (java.sql.Date) parameters.get(i));
                     } else {
                         throw new IllegalArgumentException();
                     }
@@ -96,10 +106,12 @@ public class JobRoleDao {
     }
 
     private <T> void applyFilter(
-            final List<T> values, final String key, final StringBuilder query, final List<Object> parameters) {
+            final List<T> values, final String key, final StringBuilder query,
+            final List<Object> parameters) {
         if (isPresent(values)) {
             query.append(" AND ").append(key).append(" IN (");
-            query.append(String.join(", ", Collections.nCopies(values.size(), "?")));
+            query.append(
+                    String.join(", ", Collections.nCopies(values.size(), "?")));
             query.append(")");
             parameters.addAll(values);
         }
@@ -109,7 +121,8 @@ public class JobRoleDao {
         return list != null && !list.isEmpty();
     }
 
-    private void addJobRoleFromResultSet(final List<JobRole> jobRoles, final ResultSet resultSet)
+    private void addJobRoleFromResultSet(final List<JobRole> jobRoles,
+                                         final ResultSet resultSet)
             throws ResultSetException {
         JobRole jobRole;
         try {
@@ -131,13 +144,15 @@ public class JobRoleDao {
     public JobRoleDetails getJobRoleById(final int id) throws SQLException {
         try (Connection connection = DatabaseConnector.getConnection()) {
 
-            String query = "SELECT roleName, location, capabilityName, bandName, closingDate, statusName, "
-                    + "description, responsibilities, sharepointUrl, numberOfOpenPositions\n"
-                    + "FROM job_roles\n"
-                    + "INNER JOIN capability USING(capabilityId)\n"
-                    + "INNER JOIN band USING(bandId)\n"
-                    + "INNER JOIN status using(statusId)\n"
-                    + "WHERE jobRoleId = ?;";
+            String query =
+                    "SELECT roleName, location, capabilityName, bandName, closingDate, statusName, "
+                            +
+                            "description, responsibilities, sharepointUrl, numberOfOpenPositions\n"
+                            + "FROM job_roles\n"
+                            + "INNER JOIN capability USING(capabilityId)\n"
+                            + "INNER JOIN band USING(bandId)\n"
+                            + "INNER JOIN status using(statusId)\n"
+                            + "WHERE jobRoleId = ?;";
 
             PreparedStatement statement = connection.prepareStatement(query);
 
@@ -160,5 +175,74 @@ public class JobRoleDao {
             }
         }
         return null;
+    }
+
+    public void importMultipleJobRoles(List<JobRoleDetails> detailedJobRoles)
+            throws SQLException {
+
+        String query =
+                "INSERT INTO job_roles (roleName, location, capabilityId, bandId, closingDate, description, "
+                        + "responsibilities, sharepointUrl, statusId, numberOfOpenPositions) "
+                        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+                for (JobRoleDetails jobRole : detailedJobRoles) {
+                    preparedStatement.setString(1, jobRole.getRoleName());
+                    preparedStatement.setString(2, jobRole.getJobRoleLocation());
+                    preparedStatement.setInt(3, getCapabilityIdByName(jobRole.getCapabilityName()));
+                    preparedStatement.setInt(4, getBandIdByName(jobRole.getBandName()));
+                    preparedStatement.setDate(5, jobRole.getClosingDate());
+                    preparedStatement.setString(6, jobRole.getDescription());
+                    preparedStatement.setString(7, jobRole.getResponsibilities());
+                    preparedStatement.setString(8, jobRole.getSharepointUrl());
+                    preparedStatement.setInt(9, getStatusIdByName(jobRole.getStatusName()));
+                    preparedStatement.setInt(10, jobRole.getNumberOfOpenPositions());
+
+                    preparedStatement.addBatch();
+                }
+
+                preparedStatement.executeBatch();
+            }
+    }
+
+    public int getCapabilityIdByName(String name) throws SQLException {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            String query = "SELECT capabilityId FROM capability WHERE capabilityName = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, name);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.getInt(1);
+        }
+    }
+
+    public int getBandIdByName(String name) throws SQLException {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            String query = "SELECT bandId FROM band WHERE bandName = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, name);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.getInt(1);
+        }
+    }
+
+    public int getStatusIdByName(String name) throws SQLException {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            String query = "SELECT statusApplicationId FROM application_status WHERE statusApplicationName = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, name);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.getInt(1);
+        }
     }
 }
--- src/main/java/org/example/services/JobRoleService.java
+++ src/main/java/org/example/services/JobRoleService.java
@@ -49,4 +49,6 @@ public class JobRoleService {
         }
         return jobRoleResponses;
     }
+
+
 }
